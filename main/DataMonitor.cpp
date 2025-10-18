0403beba3b12f2d78cceee6ba6fbddb78593452d
Eckhard Völlm <e.voellm@mail.de>
2025-10-18 14:42:52 +0200

    improve scope for DM logging mutex

--- main/DataMonitor.cpp
+++ main/DataMonitor.cpp
@@ -62,10 +62,11 @@ void DataMonitor::monitorString( int ch, e_dir_t dir, const char *str, int len )
 	if( !mon_started || paused || (ch != channel) ){
 		// ESP_LOGI(FNAME,"not active, return started:%d paused:%d", mon_started, paused );
 		return;
+	}else{
+		DisplayLock lock(_display);
+		bool binary = false;
+		printString( ch, dir, str, binary, len );
 	}
-	DisplayLock lock(_display);
-	bool binary = false;
-	printString( ch, dir, str, binary, len );
 }
 
 void DataMonitor::printString( int ch, e_dir_t dir, const char *str, bool binary, int len ){
